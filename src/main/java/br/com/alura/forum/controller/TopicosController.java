402243b66fe4eae6dd2e050ecd424a1f0eca06be
miguins-zup <lucas.miguins@zup.com.br>
2021-05-12 16:01:02 -0300

    Utilizando a paginação do Spring, assim podemos usar o objeto Pageable como parâmetro

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,13 +40,13 @@ public class TopicosController {
 	@Autowired private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDTO> lista(
-			@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, 
-			@RequestParam int qtd,
-			@RequestParam String ordenacao) {
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, 
+		@PageableDefault(sort = "id", direction = Direction.DESC) Pageable paginacao) {
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
+		 // Pode ser feita a utilização da paginação como parametro
+		 // É preciso colocar @EnableSpringDataWebSupport
+		 // Ou criar os parâmtros na mão:
+		 // Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
 		
 		if (nomeCurso == null) {
 			
