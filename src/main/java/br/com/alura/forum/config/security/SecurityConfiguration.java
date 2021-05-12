cb506cebd141c79963e90eb2e2247bbf224109ec
miguins-zup <lucas.miguins@zup.com.br>
2021-05-12 18:09:52 -0300

    Liberando métodos de GET

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,11 +1,36 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
+	// Configurações de auth
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
+	
+	// Configurações de Autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		// Permite apenas o métodos GET do controller de topicos
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	
+	// Configuração de recursos estaticos (css, imagens, etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
 }
